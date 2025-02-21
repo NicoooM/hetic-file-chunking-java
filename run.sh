f98b81addfa69da6ec6e7e45b02d7959da4fa706
Lucas Boucher <contact@lucasboucher.fr>
2025-02-21 09:55:15 +0100

    fix(tests): run.sh with args

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Cleaning, compiling, packaging, and running the Java project..."
-mvn clean install compile package exec:java -Dexec.mainClass="com.hetic.App"
+mvn clean install compile package exec:java -Dexec.mainClass="com.hetic.App" -Dexec.args="naza .jpeg" -DskipTests
